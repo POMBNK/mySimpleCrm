387de762844f0ba8df180702edfbd57a04544d68
Pombnk <shardex11@gmail.com>
2022-04-29 18:06:51 +0300

    First commit

---
+++ crm_leads/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11)
+
+
+class Lead(models.Model):
+    
+    CHOICES = (
+        ("VK", "VK"),
+        ("YouTube", "YouTube"),
+        ("Instagram", "Instagram"),
+        ("Twitter", "Twitter"),
+    )
+
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=18)
+
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=CHOICES, max_length=100)
+
+    profile_picture = models.ImageField(blank=True, null=True)
+    agent = models.ForeignKey("Agent",on_delete=models.CASCADE)
+
+class Agent(models.Model):
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
